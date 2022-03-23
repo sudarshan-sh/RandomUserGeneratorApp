ffa3e59e8360e8f08f7098cb60034155c117136e
Sudarshan <mail2sudarshan.sharma@gmail.com>
2022-03-23 11:11:27 +0530

    first set of development

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FaEnvelopeOpen,FaUser,FaCalendarTimes,FaMap,FaPhone,FaLock } from 'react-icons/fa';
+
+const url = 'https://randomuser.me/api/'
+const defaultImage = 'https://randomuser.me/api/portraits/lego/5.jpg'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [title, setTitle] = useState('name');
+  const [value, setValue] = useState('random person');
+
+  const getPerson = async() => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const person = data.results[0];
+    const { email,phone } = person;
+    const { first,last } = person.name;
+    const { login:{password} } = person;
+    const { large: image } = person.picture;
+    const { dob:{age} } = person;
+    const { street:{number,name} } = person.location;
+
+    const newPerson = {
+      image,email,phone,password,age,
+      street: `${number} ${name}`,
+      name: `${first} ${last}`
+    }
+    setPerson(newPerson);
+    setLoading(false);
+    setTitle('name');
+    setValue(newPerson.name);
+  }
+
+  useEffect(() => {
+    getPerson();
+  }, []);
+
+  const handleValue = (e) => {
+    if(e.target.classList.contains('icon')){
+      const newValue = e.target.dataset.label;
+      // console.log(newValue);
+      setValue(person[newValue]);
+      setTitle(newValue);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="block bcg-black"></div>
+      <div className="block">
+        <div className="container">
+          <img src={ (person && person.image) || defaultImage } alt="random person" />
+          <p className="user-title">My {title} is</p>
+          <p className="user-value">{value}</p>
+          <div className="values-list">
+            <button className="icon" data-label="name" 
+            onMouseOver={handleValue}>
+              <FaUser />
+            </button>
+            <button className="icon" data-label="email" 
+            onMouseOver={handleValue}>
+              <FaEnvelopeOpen />
+            </button>
+            <button className="icon" data-label="age" 
+            onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button className="icon" data-label="street" 
+            onMouseOver={handleValue}>
+              <FaMap />
+            </button>
+            <button className="icon" data-label="phone" 
+            onMouseOver={handleValue}>
+              <FaPhone />
+            </button>
+            <button className="icon" data-label="password" 
+            onMouseOver={handleValue}>
+              <FaLock />
+            </button>
+          </div>
+          <button type="button" className="btn" 
+          onClick={getPerson}>
+            {loading ? 'loading...' : 'random user'}
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
 
